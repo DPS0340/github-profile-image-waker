34e8df2df972495b1d1ab009ea068d13666916a4
Jiho Lee <optional.int@kakao.com>
2023-02-15 16:51:36 +0000

    chore: add quote on image link

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-    curl https://github-readme-stats-dps0340.vercel.app/api?username=DPS0340&hide=contribs&count_private=true&show_icons=true&theme=radical&include_all_commits=true&custom_title=DPS0340\'s%20Github%20Stats
+    curl "https://github-readme-stats-dps0340.vercel.app/api?username=DPS0340&hide=contribs&count_private=true&show_icons=true&theme=radical&include_all_commits=true&custom_title=DPS0340's%20Github%20Stats"
     sleep 5s
 done
