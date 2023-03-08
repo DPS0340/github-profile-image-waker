e00047e359e912b35f8ca00925eb812ec2c02701
Jiho Lee <optional.int@kakao.com>
2023-03-08 17:12:50 +0000

    chore: use implicitly wait

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 options = Options()
 options.add_argument('-headless')
 browser = webdriver.Firefox(options=options)
+browser.implicitly_wait(5)
 
 while True:
     browser.get("https://github.com/DPS0340")
